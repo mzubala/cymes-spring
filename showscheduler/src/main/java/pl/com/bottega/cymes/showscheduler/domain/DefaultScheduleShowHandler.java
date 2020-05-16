5fae3fc8da188aed78a518c8afc36ed0af4b95ae
Maciej Zubala <maciej.zubala@allegro.pl>
2020-05-16 14:41:41 +0200

    solution

--- showscheduler/src/main/java/pl/com/bottega/cymes/showscheduler/domain/DefaultScheduleShowHandler.java
+++ showscheduler/src/main/java/pl/com/bottega/cymes/showscheduler/domain/DefaultScheduleShowHandler.java
@@ -23,6 +23,17 @@ class DefaultScheduleShowHandler implements ScheduleShowHandler {
 
     @Override
     public void handle(ScheduleShowCommand command) {
-
+        var movie = movieCatalog.get(command.getMovieId());
+        var show = new Show(command, movie, configuration);
+        if(suspensionChecker.isSuspended(command.getCinemaId(), command.getCinemaHallId())) {
+            throw new CinemaHallNotAvailableException();
+        }
+        operationLocker.lock(command.getCinemaId().toString(), command.getCinemaHallId().toString(), () -> {
+            if(showRepository.anyShowsCollidingWith(show)) {
+                throw new CinemaHallNotAvailableException();
+            }
+            showRepository.save(show);
+            return null;
+        });
     }
 }
