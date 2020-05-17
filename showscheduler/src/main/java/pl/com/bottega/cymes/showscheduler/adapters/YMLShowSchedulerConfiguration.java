3b2ac0684071b1ac61e686ae6179059d57e3e21e
Maciej Zubala <maciej.zubala@allegro.pl>
2020-05-17 18:26:16 +0200

    solution

--- showscheduler/src/main/java/pl/com/bottega/cymes/showscheduler/adapters/YMLShowSchedulerConfiguration.java
+++ showscheduler/src/main/java/pl/com/bottega/cymes/showscheduler/adapters/YMLShowSchedulerConfiguration.java
@@ -1,9 +1,12 @@
 package pl.com.bottega.cymes.showscheduler.adapters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
 import pl.com.bottega.cymes.showscheduler.domain.ShowSchedulerConfiguration;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import java.io.IOException;
 import java.time.Duration;
 
 public class YMLShowSchedulerConfiguration implements ShowSchedulerConfiguration {
@@ -13,10 +16,20 @@ public class YMLShowSchedulerConfiguration implements ShowSchedulerConfiguration
     @Inject
     private ObjectMapper objectMapper;
 
+    private ShowSchedulerProps props;
 
+    @PostConstruct
+    public void init() throws IOException {
+        props = objectMapper.readValue(this.getClass().getResourceAsStream(YAML_PATH), ShowSchedulerProps.class);
+    }
 
     @Override
     public Duration showReservationBuffer() {
-        return null;
+        return props.reservationBuffer;
+    }
+
+    @Data
+    public static class ShowSchedulerProps {
+        private Duration reservationBuffer;
     }
 }
