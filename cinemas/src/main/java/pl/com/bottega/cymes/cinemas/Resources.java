734a14467a09b7b2615f0febcce69b2e7069ae53
Maciej Zubala <maciej.zubala@allegro.pl>
2020-10-17 20:35:30 +0200

    prep

--- cinemas/src/main/java/pl/com/bottega/cymes/cinemas/Resources.java
+++ cinemas/src/main/java/pl/com/bottega/cymes/cinemas/Resources.java
@@ -1,5 +1,8 @@
 package pl.com.bottega.cymes.cinemas;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -9,5 +12,12 @@ public class Resources {
     @PersistenceContext
     @Produces
     private EntityManager em;
+
+    @Produces
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
 
 }
