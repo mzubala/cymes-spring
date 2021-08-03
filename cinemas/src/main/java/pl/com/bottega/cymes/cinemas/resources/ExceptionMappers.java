02bd0869d9c09414ae3d573885ca4337567eb499
Maciej Zubala <maciej.zubala@allegro.pl>
2021-08-03 19:34:33 +0200

    task prep

--- cinemas/src/main/java/pl/com/bottega/cymes/cinemas/resources/ExceptionMappers.java
+++ cinemas/src/main/java/pl/com/bottega/cymes/cinemas/resources/ExceptionMappers.java
@@ -2,33 +2,33 @@ package pl.com.bottega.cymes.cinemas.resources;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.com.bottega.cymes.cinemas.dataaccess.dao.GenericDao;
 
 import javax.persistence.EntityNotFoundException;
-import javax.validation.ConstraintViolation;
-import java.util.Map;
 import java.util.Set;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.mapping;
-import static java.util.stream.Collectors.toSet;
-
 @ControllerAdvice
-public class ExceptionMappers {
+public class ExceptionMappers  extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
 
     @ExceptionHandler(GenericDao.EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Error> handleEntityNotFoundException(GenericDao.EntityNotFoundException ex) {
         return new ResponseEntity<>(new Error(ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Error> handleJPAEntityNotFoundException(EntityNotFoundException ex) {
         return new ResponseEntity<>(new Error(ex), HttpStatus.NOT_FOUND);
     }
@@ -44,31 +44,15 @@ public class ExceptionMappers {
     }
 
     @Data
+    @AllArgsConstructor
     public static class ValidationErrors {
-
         private Set<ValidationError> errors;
-
-        public ValidationErrors(Set<ConstraintViolation<?>> violations) {
-            Map<String, Set<String>> errorsMap = violations.stream().collect(
-                groupingBy(
-                    (violation) -> violation.getPropertyPath().toString(), mapping(ConstraintViolation::getMessage, toSet())
-                )
-            );
-            this.errors = errorsMap.entrySet().stream()
-                .map((entry) -> new ValidationError(entry.getKey(), entry.getValue()))
-                .collect(toSet());
-        }
-
     }
 
     @Data
+    @AllArgsConstructor
     public static class ValidationError {
-        private Set<String> messages;
+        private String message;
         private String field;
-
-        ValidationError(String field, Set<String> messages) {
-            this.messages = messages;
-            this.field = field;
-        }
     }
 }
