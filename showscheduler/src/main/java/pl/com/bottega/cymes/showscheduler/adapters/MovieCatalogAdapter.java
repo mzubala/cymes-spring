3895814a88dbd3443aed254a7283081a3a255eec
Maciej Zubala <maciej.zubala@allegro.pl>
2021-08-11 13:10:02 +0200

    task prep

--- showscheduler/src/main/java/pl/com/bottega/cymes/showscheduler/adapters/MovieCatalogAdapter.java
+++ showscheduler/src/main/java/pl/com/bottega/cymes/showscheduler/adapters/MovieCatalogAdapter.java
@@ -1,8 +1,8 @@
 package pl.com.bottega.cymes.showscheduler.adapters;
 
 import lombok.Data;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import pl.com.bottega.cymes.showscheduler.domain.Movie;
@@ -13,13 +13,16 @@ import pl.com.bottega.cymes.showscheduler.domain.MovieCatalog;
 public class MovieCatalogAdapter implements MovieCatalog {
 
     private final WebClient moviesClient;
+    private final ReactiveCircuitBreakerFactory cbFactory;
 
     @Override
     public Movie get(Long movieId) {
         return moviesClient
                 .get()
                 .uri("/movies/{movieId}", movieId)
-                .exchangeToMono((response) -> response.bodyToMono(MovieResponse.class).map(MovieResponse::toDomain))
+                .retrieve().bodyToMono(MovieResponse.class)
+                .transform(it -> cbFactory.create("movies-circuit-breaker").run(it))
+                .map(MovieResponse::toDomain)
                 .block();
     }
 }
