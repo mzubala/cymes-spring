3cd1425837131c4650088aa44fcbe3c1c1c0642d
Maciej Zubala <maciej.zubala@allegro.pl>
2021-08-11 13:10:02 +0200

    task prep

--- showscheduler/src/test/java/pl/com/bottega/cymes/showscheduler/integration/MovieCatalogAdapterTest.java
+++ showscheduler/src/test/java/pl/com/bottega/cymes/showscheduler/integration/MovieCatalogAdapterTest.java
@@ -1,18 +1,24 @@
 package pl.com.bottega.cymes.showscheduler.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.com.bottega.cymes.showscheduler.adapters.MovieCatalogAdapter;
 import pl.com.bottega.cymes.showscheduler.domain.Movie;
 
-import javax.inject.Inject;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.lessThan;
+import static com.github.tomakehurst.wiremock.client.WireMock.moreThan;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @IntegrationTest
@@ -21,7 +27,15 @@ public class MovieCatalogAdapterTest {
     @Autowired
     private MovieCatalogAdapter movieCatalogAdapter;
 
+    @Autowired
+    private CircuitBreakerRegistry circuitBreakerRegistry;
+
     private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void resetCircuitBreaker() {
+        circuitBreakerRegistry.getAllCircuitBreakers().forEach(cb -> cb.reset());
+    }
 
     @Test
     public void returnsMovie() {
@@ -36,6 +50,26 @@ public class MovieCatalogAdapterTest {
         assertThat(fetchedMovie).isEqualTo(movie);
     }
 
+    @Test
+    public void usesACircuitBreaker() {
+        // given
+        stubMoviesServiceFailure();
+        int n = 200;
+
+        // when
+        for(int i = 0; i<n; i++) {
+            try {
+                movieCatalogAdapter.get((long) i);
+            } catch (Exception ex) {
+
+            }
+        }
+
+        // then
+        verify(moreThan(0), getRequestedFor(urlPathMatching("/movies/(.*)")));
+        verify(lessThan(n), getRequestedFor(urlPathMatching("/movies/(.*)")));
+    }
+
     @SneakyThrows
     private void stubMovie(Movie movie) {
         stubFor(get(urlPathEqualTo("/movies/" + movie.getId()))
@@ -43,5 +77,12 @@ public class MovieCatalogAdapterTest {
                 .withBody(objectMapper.writeValueAsString(movie))
                 .withHeader("Content-type", "application/json")
             ));
+    }
+
+    private void stubMoviesServiceFailure() {
+        stubFor(get(urlPathMatching("/movies/(.*)"))
+                .willReturn(aResponse()
+                        .withStatus(500)
+                ));
     }
 }
