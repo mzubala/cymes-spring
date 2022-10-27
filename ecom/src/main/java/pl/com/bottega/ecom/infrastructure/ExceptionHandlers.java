4cde54644a33293df82d70f4072cf1e99d7aeb52
Maciej Zubala <maciej.zubala@gmail.com>
2022-10-27 15:08:51 +0200

    better error messaging

--- ecom/src/main/java/pl/com/bottega/ecom/infrastructure/ExceptionHandlers.java
+++ ecom/src/main/java/pl/com/bottega/ecom/infrastructure/ExceptionHandlers.java
@@ -3,15 +3,21 @@ package pl.com.bottega.ecom.infrastructure;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-class ExceptionHandlers {
+class ExceptionHandlers extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     ResponseEntity<GlobalError> handleEntityNotFoundException(EntityNotFoundException ex) {
@@ -26,10 +32,33 @@ class ExceptionHandlers {
         throw ex;
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(new ValidationErrors(
+            ex.getFieldErrors().stream()
+                .map(fieldError -> new ValidationError(fieldError.getField(), fieldError.getDefaultMessage()))
+                .collect(Collectors.toList())
+        ), HttpStatus.BAD_REQUEST);
+
+    }
+
     @Data
     @AllArgsConstructor
     static class GlobalError {
         String errorMessage;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ValidationErrors {
+        List<ValidationError> errors;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ValidationError {
+        String fieldName;
+        String message;
     }
 
 }
