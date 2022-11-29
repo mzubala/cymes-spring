53ed2e1f14bb4b507158b042b85b1074e718769b
Maciej Zubala <maciej.zubala@gmail.com>
2022-11-29 17:14:02 +0100

    spring boot 3.0 migration

--- ecom/src/main/java/pl/com/bottega/ecom/user/User.java
+++ ecom/src/main/java/pl/com/bottega/ecom/user/User.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import java.util.UUID;
 
 @Entity
