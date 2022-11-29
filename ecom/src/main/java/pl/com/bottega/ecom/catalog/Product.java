8f81e0e747c79ed8c170a1707e5c2d4b7cb69259
Maciej Zubala <maciej.zubala@gmail.com>
2022-11-29 16:59:30 +0100

    fixed jpa model

--- ecom/src/main/java/pl/com/bottega/ecom/catalog/Product.java
+++ ecom/src/main/java/pl/com/bottega/ecom/catalog/Product.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import java.util.UUID;
 
 @Entity
