dcd0aad2f0471cdb373fc764a17b3d60d52b2249
Maciej Zubala <maciej.zubala@gmail.com>
2022-11-29 17:14:02 +0100

    spring boot 3.0 migration

--- ecom/src/main/java/pl/com/bottega/ecom/catalog/ProductController.java
+++ ecom/src/main/java/pl/com/bottega/ecom/catalog/ProductController.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
