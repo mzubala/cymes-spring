980050ea6e210219a295d52c8dae6beeee149de8
Maciej Zubala <maciej.zubala@gmail.com>
2022-11-29 17:14:02 +0100

    spring boot 3.0 migration

--- ecom/src/main/java/pl/com/bottega/ecom/user/UserController.java
+++ ecom/src/main/java/pl/com/bottega/ecom/user/UserController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 @RestController
