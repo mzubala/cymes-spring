642aa3e9288a5ffce8f65bf4642c6b7df914e514
Maciej Zubala <maciej.zubala@gmail.com>
2022-11-30 13:05:01 +0100

    spring boot 3 migration

--- cinemas/src/main/java/pl/com/bottega/cymes/cinemas/resources/ExceptionMappers.java
+++ cinemas/src/main/java/pl/com/bottega/cymes/cinemas/resources/ExceptionMappers.java
@@ -1,9 +1,11 @@
 package pl.com.bottega.cymes.cinemas.resources;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +14,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.com.bottega.cymes.cinemas.dataaccess.dao.GenericDao;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -22,7 +23,7 @@ import static java.util.stream.Collectors.toSet;
 public class ExceptionMappers extends ResponseEntityExceptionHandler {
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         Stream<ValidationError> fieldErrors = ex.getBindingResult().getFieldErrors().stream().map((error) ->
                 new ValidationError(error.getField(), error.getDefaultMessage())
         );
