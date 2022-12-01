9b39df8e47c30018ee549fbe6d35b89515a4c613
Maciej Zubala <maciej.zubala@gmail.com>
2022-12-01 10:07:52 +0100

    spring update

--- movies/src/main/java/pl/com/bottega/cymes/movies/model/Movie.java
+++ movies/src/main/java/pl/com/bottega/cymes/movies/model/Movie.java
@@ -2,13 +2,12 @@ package pl.com.bottega.cymes.movies.model;
 
 import lombok.Data;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import java.util.List;
 import java.util.Set;
 
