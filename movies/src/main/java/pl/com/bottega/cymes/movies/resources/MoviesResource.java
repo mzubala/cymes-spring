e3c5a30fac2242d09cc814bccd629022da107f99
Maciej Zubala <maciej.zubala@gmail.com>
2022-12-01 10:07:52 +0100

    spring update

--- movies/src/main/java/pl/com/bottega/cymes/movies/resources/MoviesResource.java
+++ movies/src/main/java/pl/com/bottega/cymes/movies/resources/MoviesResource.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Set;
 
