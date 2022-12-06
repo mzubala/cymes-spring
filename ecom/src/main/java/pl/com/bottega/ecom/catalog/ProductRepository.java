76cf219fdb6eeaa5b77ee4aecb2dfb95b8e205bf
Maciej Zubala <maciej.zubala@gmail.com>
2022-12-06 14:15:38 +0100

    metamodel

--- ecom/src/main/java/pl/com/bottega/ecom/catalog/ProductRepository.java
+++ ecom/src/main/java/pl/com/bottega/ecom/catalog/ProductRepository.java
@@ -1,13 +1,8 @@
 package pl.com.bottega.ecom.catalog;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.repository.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,7 +18,7 @@ class ProductSpecifications {
     static Specification<Product> nameMatches(String phrase) {
         return (root, query, criteriaBuilder) -> {
             if (phrase != null) {
-                return criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + phrase.toLowerCase() + "%");
+                return criteriaBuilder.like(criteriaBuilder.lower(root.get(Product_.name)), "%" + phrase.toLowerCase() + "%");
             }
             return null;
         };
@@ -32,7 +27,7 @@ class ProductSpecifications {
     static Specification<Product> categoryIdEquals(UUID categoryId) {
         return (root, query, criteriaBuilder) -> {
             if(categoryId != null) {
-                return criteriaBuilder.equal(root.get("category").get("id"), categoryId);
+                return criteriaBuilder.equal(root.get(Product_.category).get(Category_.id), categoryId);
             }
             return null;
         };
