667e615a695665c3984bcfcf461facaf56afbc9d
Maciej Zubala <maciej.zubala@gmail.com>
2022-12-07 08:46:14 +0100

    prep

---
+++ ecom/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into "user" ("id", "email", "password") values ('386e8098560511edbdc30242ac120002', 'test@test.com', '123456');
+insert into "category" ("id", "name") values ('21cdefb5c989448a9ac1b5c3ee8d18d5', 'Jedzenie');
+insert into "product" ("id", "name", "price", "category_id") values ('6ba02c4e04ae4321bda56bb4def03086', 'Masło', 5.50, '21cdefb5c989448a9ac1b5c3ee8d18d5');
